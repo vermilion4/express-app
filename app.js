f5170570b9e40c6fda72287f3eba3fef838ec934
Ndupu Adaeze <andupu4@gmail.com>
2019-05-23 15:06:57 +0100

    add cor

--- app.js
+++ app.js
@@ -27,7 +27,11 @@ err => {
 }
 );
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
  
