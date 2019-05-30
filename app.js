efebb7df2c176a0e94f5db9f9ef5ff3db57fb105
Ndupu Adaeze <andupu4@gmail.com>
2019-05-30 10:24:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json())
 // allow cross origin request
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS")
+  res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "PATCH")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
